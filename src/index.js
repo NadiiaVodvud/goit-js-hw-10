9026540e7b94e35d767bafd69b4104ef603c2170
NadiiaVodvud <108605727+NadiiaVodvud@users.noreply.github.com>
2022-12-14 22:01:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function createCountriesItemsMarcup(countries) {
   } else if (Number(countries.length) !== 1) {
     const markup = countries
       .map(({ name, flags }) => {
-        return `<li class="country-item"><img class="country-img" src="${flags.svg}" alt="flag of ${name.official}" width="50" height="100%"><p class="country-title">${name.official}</p></li>`;
+        return `<li class="country-item"><img class="country-img" src="${flags.svg}" alt="flag of ${name.common}" width="50" height="100%"><p class="country-title">${name.common}</p></li>`;
       })
       .join('');
     refs.countryList.innerHTML = markup;
@@ -47,9 +47,9 @@ function createCountriesItemsMarcup(countries) {
         return `<div class="country-card"><div class="country-card-box"><img class="country-card-svg"  src="${
           flags.svg
         }"  alt="flag of ${
-          name.official
+          name.common
         }" width="70" heiht="100%"><p class="country-card-title">${
-          name.official
+          name.common
         }</p></div><p class="country-card-text">Capital:<span class="country-card-topic">${capital}</span></p><p class="country-card-text">Population:<span class="country-card-topic">${population}</span></p><p class="country-card-text">Languages:<span class="country-card-topic">${Object.values(
           languages
         )}</span></p></div>`;
@@ -83,30 +83,35 @@ function onFetchError() {
 //   }
 // }
 
+// function createCountriesList(country) {
+//   return `<li class="country-item"><img class="country-img" src="${flags.svg}" alt="flag of ${name.common}" width="50"><p class="country-title">${name.common}</p></li>`;
+// }
+
 // function renderCountriesList(countries) {
 //   const markup = countries
-//     .map(({ name, flags }) => {
-//       return `<li class="country-item"><img class="country-img" src="${flags.svg}" alt="flag of ${name.official}" width="50"><p class="country-title">${name.official}</p></li>`;
-//     })
+//     .map(({ name, flags }) => createCountriesList(country))
 //     .join('');
-//   // refs.countryList.innerHTML = markup;
 //   refs.countryList.insertAdjacentElement('beforeend', markup);
+// }
+
+// function createCountriesCard() {
+//   return `<div class="country-card"><div class="country-card-box"><img class="country-card-svg"  src="${
+//     flags.svg
+//   }"  alt="flag of ${
+//     name.common
+//   }" width="70" heiht="100%"><p class="country-card-title">${
+//     name.common
+//   }</p></div><p class="country-card-text">Capital:<span class="country-card-topic">${capital}</span></p><p class="country-card-text">Population:<span class="country-card-topic">${population}</span></p><p class="country-card-text">Languages:<span class="country-card-topic">${Object.values(
+//     languages
+//   )}</span></p></div>`;
 // }
 
 // function renderCountriesCard(countries) {
 //   const markup = countries
-//     .map(({ name, flags, capital, population, languages }) => {
-//       return `<div class="country-card"><div class="country-card-box"><img class="country-card-svg"  src="${
-//         flags.svg
-//       }"  alt="flag of ${
-//         name.official
-//       }" width="70" heiht="100%"><p class="country-card-title">${
-//         name.official
-//       }</p></div><p class="country-card-text">Capital:<span class="country-card-topic">${capital}</span></p><p class="country-card-text">Population:<span class="country-card-topic">${population}</span></p><p class="country-card-text">Languages:<span class="country-card-topic">${Object.values(
-//         languages
-//       )}</span></p></div>`;
-//     })
+//     .map(({ name, flags, capital, population, languages }) =>
+//       createCountriesCard(country)
+//     )
 //     .join('');
-//   // refs.infoContainer.innerHTML = markup;
+
 //   refs.infoContainer.insertAdjacentElement('beforeend', markup);
 // }
